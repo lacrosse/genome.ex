9096cdbdc5a5842aa27f88d74383dee9dd83ab9d
Alexander Skiba <alex@foremal.com>
2017-02-02 01:55:20 +0700

    Add Genome.Sequence tests

--- lib/genome/sequence.ex
+++ lib/genome/sequence.ex
@@ -2,9 +2,9 @@ defmodule Genome.Sequence do
   alias Genome.Nucleotide
 
   def pattern_count("", _), do: 0
-  def pattern_count(genome, pattern) do
+  def pattern_count(seq, pattern) do
     pattern_size = String.length(pattern)
-    case genome do
+    case seq do
       <<^pattern::binary-size(pattern_size), tail::binary>> ->
         <<_, pre_tail::binary>> = pattern
         1 + pattern_count(pre_tail <> tail, pattern)
@@ -13,13 +13,13 @@ defmodule Genome.Sequence do
     end
   end
 
-  def frequent_patterns(genome, k) do
-    patterns = for i <- 0..String.length(genome) - k - 1, do: String.slice(genome, i, k)
+  def frequent_patterns(seq, k) do
+    patterns = for i <- 0..String.length(seq) - k - 1, do: String.slice(seq, i, k)
 
     frequencies =
       patterns
       |> Enum.reduce(Map.new, fn pattern, freqs ->
-        Map.put_new_lazy(freqs, pattern, fn -> pattern_count(genome, pattern) end)
+        Map.put_new_lazy(freqs, pattern, fn -> pattern_count(seq, pattern) end)
       end)
 
     {_, top_frequency} =
@@ -31,26 +31,27 @@ defmodule Genome.Sequence do
     |> Enum.filter_map(&elem(&1, 1) == top_frequency, &elem(&1, 0))
   end
 
-  def faster_frequent_patterns(genome, k) do
-    genome
+  def faster_frequent_patterns(seq, k) do
+    seq
     |> frequencies(k)
     |> Enum.reduce({[], 0}, fn
       {encoded_pattern, count}, {_, winning_count} when count > winning_count ->
-        {[decode(encoded_pattern, k), count]}
+        {[decode(encoded_pattern, k)], count}
       {encoded_pattern, count}, {patterns, count} ->
         {[decode(encoded_pattern, k)|patterns], count}
       _, acc ->
         acc
     end)
+    |> elem(0)
   end
 
   def reverse_complement(""), do: ""
   def reverse_complement(<<nucleotide::binary-size(1), tail::binary>>),
     do: reverse_complement(tail) <> Nucleotide.reverse(nucleotide)
 
-  def pattern_matches(genome, pattern), do: do_pattern_matches(genome, pattern, 0, [])
+  def pattern_matches(seq, pattern), do: do_pattern_matches(seq, pattern, 0, [])
 
-  def encode(genome, acc \\ "")
+  def encode(seq, acc \\ "")
   def encode("", acc), do: acc |> String.to_integer(4)
   def encode(<<nucleotide::binary-1, tail::binary>>, acc),
     do: encode(tail, acc <> Integer.to_string(Nucleotide.encode(nucleotide)))
@@ -65,17 +66,17 @@ defmodule Genome.Sequence do
     |> Enum.join()
   end
 
-  def frequencies(genome, k, acc \\ %{})
-  def frequencies(genome, k, acc) do
-    with <<kmer::binary-size(k), _::binary>> <- genome do
-      frequencies(String.slice(genome, 1..-1), k, Map.update(acc, encode(kmer), 1, & &1 + 1))
+  def frequencies(seq, k, acc \\ %{})
+  def frequencies(seq, k, acc) do
+    with <<kmer::binary-size(k), _::binary>> <- seq do
+      frequencies(String.slice(seq, 1..-1), k, Map.update(acc, encode(kmer), 1, & &1 + 1))
     else
       _ -> acc
     end
   end
 
-  def find_clumps(genome, k, l, saturation) do
-    genome
+  def find_clumps(seq, k, l, saturation) do
+    seq
     |> to_charlist()
     |> Stream.chunk(l, 1)
     |> Stream.map(& &1 |> to_string() |> frequencies(k))
@@ -85,14 +86,14 @@ defmodule Genome.Sequence do
   end
 
   defp do_pattern_matches("", _, _, acc), do: acc
-  defp do_pattern_matches(genome, pattern, index, acc) do
+  defp do_pattern_matches(seq, pattern, index, acc) do
     pattern_size = String.length(pattern)
     new_acc =
-      case genome do
+      case seq do
         <<^pattern::binary-size(pattern_size), _::binary>> -> [index|acc]
         _ -> acc
       end
-    <<_, tail::binary>> = genome
+    <<_, tail::binary>> = seq
     do_pattern_matches(tail, pattern, index + 1, new_acc)
   end
 end
--- test/genome_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GenomeTest do
-  use ExUnit.Case
-  doctest Genome
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
